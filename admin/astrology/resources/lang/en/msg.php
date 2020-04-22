84678c70e03bc89ecbb0ed3ab651837216913a4c
Bikas Pandey <43566436+pandeybikas@users.noreply.github.com>
2020-04-23 00:26:05 +0530

    edit
    edit

--- admin/astrology/resources/lang/en/msg.php
+++ admin/astrology/resources/lang/en/msg.php
@@ -16,7 +16,13 @@ return [
 'Monthly'=>'Monthly',
 'Yearly'=>'Yearly',
 'Yearly'=>'Yearly',
-'Misc'=>'Misc'
+'Misc'=>'Misc',
+'India is a country that is famous all over the world for the unity it possesses in spite of its diverse culture. It is a country that celebrates almost every major festival that exists in this world. Festivals of Hinduism, Islam, Christianity, Sikhism, Buddhism and what not. 
+In India, every festival is celebrated with a great deal of excitement and enthusiasm. But, with so many festivals, it is no surprise that it becomes kind of difficult to keep track of every festival. In order to make it easier for you, we have brought Indian calendar of the year 2020
+In this calendar, every necessary information about important festivals and government holidays is provided for you.'=>'India is a country that is famous all over the world for the unity it possesses in spite of its diverse culture. It is a country that celebrates almost every major festival that exists in this world. Festivals of Hinduism, Islam, Christianity, Sikhism, Buddhism and what not. 
+In India, every festival is celebrated with a great deal of excitement and enthusiasm. But, with so many festivals, it is no surprise that it becomes kind of difficult to keep track of every festival. In order to make it easier for you, we have brought Indian calendar of the year 2020
+In this calendar, every necessary information about important festivals and government holidays is provided for you.'
+
 ]
 
 
--- admin/astrology/resources/lang/hi/msg.php
+++ admin/astrology/resources/lang/hi/msg.php
@@ -16,6 +16,11 @@ return [
 'Monthly'=>'मासिक',
 'Yearly'=>'वार्षिक',
 'Misc'=>'विविध',
+'India is a country that is famous all over the world for the unity it possesses in spite of its diverse culture. It is a country that celebrates almost every major festival that exists in this world. Festivals of Hinduism, Islam, Christianity, Sikhism, Buddhism and what not. 
+In India, every festival is celebrated with a great deal of excitement and enthusiasm. But, with so many festivals, it is no surprise that it becomes kind of difficult to keep track of every festival. In order to make it easier for you, we have brought Indian calendar of the year 2020
+In this calendar, every necessary information about important festivals and government holidays is provided for you.'=>'भारत एक ऐसा देश है जो अपनी विविध संस्कृति के बावजूद एकता के लिए दुनिया भर में प्रसिद्ध है। यह एक ऐसा देश है जो इस दुनिया में मौजूद लगभग हर बड़े त्योहार को मनाता है। हिंदू धर्म, इस्लाम, ईसाई धर्म, सिख धर्म, बौद्ध धर्म और क्या नहीं के त्योहार।
+भारत में, हर त्योहार बहुत उत्साह और उत्साह के साथ मनाया जाता है। लेकिन, इतने सारे त्योहारों के साथ, यह कोई आश्चर्य की बात नहीं है कि हर त्योहार पर नज़र रखना मुश्किल हो जाता है। आपके लिए इसे आसान बनाने के लिए, हम वर्ष 2020 का भारतीय कैलेंडर लेकर आए हैं
+इस कैलेंडर में, आपके लिए महत्वपूर्ण त्योहारों और सरकारी छुट्टियों के बारे में हर आवश्यक जानकारी प्रदान की जाती है।',
 ]
 
 
